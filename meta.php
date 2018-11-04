4467dbe837318e2012288447d4d56a34b5c3cbba
jamiecreated <jamie.4444@yahoo.co.uk>
2018-11-04 23:05:45 +0000

    Add files via upload

---
+++ meta.php
@@ -0,0 +1,7 @@
+    <meta charset="UTF-8">
+    <meta title="Sandridge Primary School | Home Page"
+    <meta name="description" content="Welcome to Sandridge Primary School">
+    <meta name="keywords" content="Hertfordshire Primary School, Sandridge">
+    <meta name="author" content="JF Formation">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="sandridge_primary_school.css">
